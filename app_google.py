fe4d992a6d14886ec19321bc652cb6e8ec5fd260
trungvpdkddsl-ai <trungvpdkddsl@gmail.com>
2025-11-26 15:53:24 +0700

    Update app_google.py

--- app_google.py
+++ app_google.py
@@ -12,8 +12,8 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseUpload
 
 # --- 1. CẤU HÌNH ---
-TELEGRAM_TOKEN = ""
-TELEGRAM_CHAT_ID = ""
+TELEGRAM_TOKEN = "8514665869:AAHUfTHgNlEEK_Yz6yYjZa-1iR645Cgr190"
+TELEGRAM_CHAT_ID = "-5046493421"
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 ROLES = ["Quản lý", "Nhân viên", "Chưa cấp quyền"]
@@ -584,3 +584,4 @@ else:
                         st.toast("Lưu!")
                         st.rerun()
         else: st.error("Cấm truy cập!")
+
